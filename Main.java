2e373079a20d249c369fe18f737b120f91e31e75
LuoChen992 <670124603@qq.com>
2020-07-20 00:28:27 +0800

    day 7-20

--- Main.java
+++ Main.java
@@ -1,9 +1,6 @@
 package com.company;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.Set;
-import java.util.Stack;
+import java.util.*;
 
 //通过Set集合的特性使得每个加不进集合里的数字就是重复的数字
 class offer031 {
@@ -127,6 +124,119 @@ class offer062 {
         int[] print = new int[size];
         for (int i = 0; i < size; i++) {
             print[i] = stack.pop();
+        }
+        return print;
+    }
+}
+//通过前序遍历和中序遍历根据递推来重建二叉树
+//具体就是通过每一次得到根节点
+class offer071 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    HashMap<Integer,Integer> dic=new HashMap<>();
+    int []po;
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        po=preorder;
+        for(int i = 0; i <inorder.length ; i++) {
+            dic.put(inorder[i],i);
+        }
+        return recur(0,0,inorder.length-1);
+    }
+    TreeNode recur(int pre_root, int in_left, int in_right) {
+        if(in_left>in_right)return null;
+        TreeNode root=new TreeNode(po[pre_root]);
+        int i=dic.get(po[pre_root]); //中序遍历中的根节点的索引
+        root.left=recur(pre_root+1,in_left,i-1);
+        root.right=recur(pre_root+1+i-in_left,i+1,in_right);
+        //这里的右子树的根节点索引:到前序遍历里面去看,
+        return root;
+    }
+}
+//二叉树镜像方法1：用递归的方法
+class offer271 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode mirrorTree(TreeNode root) {
+        TreeNode temp=new TreeNode(0);
+        if (root==null){
+            return null;
+        }
+        temp=root.left;
+        root.left=mirrorTree(root.right);
+        root.right=mirrorTree(root.left);
+        return root;
+    }
+}
+//二叉树镜像方法2：用队列或栈的方法
+class offer272 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode(int x) { val = x; }
+    }
+    public TreeNode mirrorTree(TreeNode root) {
+
+        if (root==null){
+            return null;
+        }
+        Stack<TreeNode> stack = new Stack<>() ;
+        stack.add(root);
+        while (!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            if(node.left != null) stack.add(node.left);
+            if(node.right != null) stack.add(node.right);
+            TreeNode tmp = node.left;
+            node.left = node.right;
+            node.right = tmp;
+        }
+        return root;
+    }
+}
+
+//顺时针打印
+class offer291 {
+    public int[] spiralOrder(int[][] matrix) {
+        if(matrix.length==0)return new int[0];
+        int l=0,r=matrix[0].length-1,t=0,b= matrix.length-1;
+        int x=0;
+        int[] print=new int[(r+1)*(b+1)];
+        while (true){
+            //第一行从左往右
+            for (int i = l; i <=r ; i++) {
+                print[x++]=matrix[t][i];
+            }
+            t++;  //将top往下移一层
+            if(t>b)break;
+
+            //第二次 从上到下
+            for (int i = t; i <= b; i++) {
+                print[x++]=matrix[i][r];
+            }
+            r--;
+            if(r<l)break;
+
+            //第三次 从右往左
+            for (int i = r; i >= l; i--) {
+                print[x++]=matrix[b][i];
+            }
+            b--;
+            if(b<t)break;
+
+            //
+            for (int i = b; i >=t ; i--) {
+                print[x++]=matrix[i][l];
+            }
+            l++;
+            if(l>r)break;
         }
         return print;
     }
