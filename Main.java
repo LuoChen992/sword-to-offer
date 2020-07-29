9788fe773635bda6eb546e148166cda098d99cd5
LuoChen992 <670124603@qq.com>
2020-07-29 23:18:14 +0800

    day 7.29

--- Main.java
+++ Main.java
@@ -219,6 +219,29 @@ class offer111 {
         return numbers[0];
     }
 }
+//12运动路径
+class offer121 {
+    public boolean exist(char[][] board, String word) {
+        char[] words = word.toCharArray();
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < board[0].length; j++) {
+                if(dfs(board, words, i, j, 0)) return true;
+            }
+        }
+        return false;
+    }
+    boolean dfs(char[][] board, char[] word, int i, int j, int k) {
+        if(i >= board.length || i < 0 || j >= board[0].length || j < 0 || board[i][j] != word[k]) return false;
+        if(k == word.length - 1) return true;
+        char tmp = board[i][j];
+        board[i][j] = '/';
+        boolean res = dfs(board, word, i + 1, j, k + 1) || dfs(board, word, i - 1, j, k + 1) ||
+                dfs(board, word, i, j + 1, k + 1) || dfs(board, word, i , j - 1, k + 1);
+        board[i][j] = tmp;
+        return res;
+    }
+}
+
 //二叉树镜像方法1：用递归的方法
 class offer271 {
     public class TreeNode {
